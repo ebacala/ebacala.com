01f71dd1e842706f66f763a07b130916788fcaa8
Evann BACALA <dev@evannbacala.fr>
2023-03-10 16:07:58 +0100

    feat: homepage

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  typescript: {
+    shim: false
+  },
+  css:['@/assets/css/reset.min.css', '@/assets/css/main.scss']
+})
