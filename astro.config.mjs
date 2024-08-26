563425a6d4c36053e9dabeb562321d5706b247b3
Evann BACALA <d3v@ebacala.com>
2024-08-26 22:04:37 +1200

    ⬆️ Upgraded dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-import { defineConfig, squooshImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: squooshImageService()
-  },
   site: 'https://ebacala.com',
   integrations: [sitemap(), tailwind()]
 });
