f68ec28e560360364fe6288f3d80fcf4dadaf352
Evann BACALA <d3v@ebacala.com>
2024-08-28 18:31:40 +1200

    💄 Changed some style + prepared for the future code blocks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,105 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+
+import { h } from "hastscript";
+
+import mdx from "@astrojs/mdx";
+
+import {
+  transformerNotationDiff,
+} from "@shikijs/transformers";
+
+/**
+ * This transformer is used to extract the meta information from the markdown file.
+ * Example:
+ * 
+ * &#96;&#96;&#96; lang title=test
+ * 
+ * console.log('Hello, world!');
+ * 
+ * &#96;&#96;&#96;
+ * 
+ * Returns { title: 'test' }
+ * @returns 
+ */
+const transformerMeta = () => ({
+  name: "transformer-meta",
+  pre() {
+    const metaRaw = this.options.meta?.__raw;
+    let meta = {};
+    if (metaRaw) {
+      const parts = metaRaw.split(/\s+/);
+      for (const part of parts) {
+        const [key, value] = part.split('=');
+        if (key && value) {
+          meta[key] = value;
+        }
+      }
+    }
+    this.meta = meta;
+  },
+});
+
+/**
+ * This transformer is used to add a copy button to the code blocks. 
+ */
+const transformerCopyButton = () => ({
+  name: "transformer-color-lines",
+  pre(node) {
+    const preHeaderDiv = h("div", {
+      class: "pre-header",
+    });
+
+    const copyCodeButton = h(
+      "div",
+      {
+        class: "wrapper-copy-code"
+      },
+      h(
+        "button",
+        {
+          class: "copy-code",
+          "data-code": this.source,
+          onclick: `
+          navigator.clipboard.writeText(this.dataset.code);
+          this.textContent = 'Copied!';
+          setTimeout(() => this.textContent = 'Copy', 1000)
+        `,
+        },
+        "Copy"
+      )
+    );
+
+    node.children.unshift(preHeaderDiv);
+
+    if (this.meta.title) {
+      const titleDiv = h("div", {
+        class: "pre-title",
+      }, this.meta.title);
+      
+      preHeaderDiv.children.push(titleDiv);
+    }
+
+    if (this.meta.copy) {
+      preHeaderDiv.children.push(copyCodeButton);
+    }
+  },
+});
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ebacala.com',
-  integrations: [sitemap(), tailwind()]
+  site: "https://ebacala.com",
+  integrations: [sitemap(), tailwind(), mdx()],
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+      transformers: [
+        transformerMeta(),
+        transformerCopyButton(),
+        transformerNotationDiff(),
+      ],
+      wrap: true,
+    },
+  },
 });
