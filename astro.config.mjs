61b3a8e7229360279c36ae0cd380ceaf347d50c7
Evann BACALA <d3v@ebacala.com>
2024-08-31 18:17:01 +1200

    ⚡️ Removed the need for MDX + broken up transformers for better readability

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,20 @@ import tailwind from "@astrojs/tailwind";
 
 import { h } from "hastscript";
 
-import mdx from "@astrojs/mdx";
-
-import {
-  transformerNotationDiff,
-} from "@shikijs/transformers";
+import { transformerNotationDiff } from "@shikijs/transformers";
 
 /**
  * This transformer is used to extract the meta information from the markdown file.
  * Example:
- * 
+ *
  * &#96;&#96;&#96; lang title=test
- * 
+ *
  * console.log('Hello, world!');
- * 
+ *
  * &#96;&#96;&#96;
- * 
+ *
  * Returns { title: 'test' }
- * @returns 
+ * @returns
  */
 const transformerMeta = () => ({
   name: "transformer-meta",
@@ -31,7 +27,7 @@ const transformerMeta = () => ({
     if (metaRaw) {
       const parts = metaRaw.split(/\s+/);
       for (const part of parts) {
-        const [key, value] = part.split('=');
+        const [key, value] = part.split("=");
         if (key && value) {
           meta[key] = value;
         }
@@ -42,46 +38,70 @@ const transformerMeta = () => ({
 });
 
 /**
- * This transformer is used to add a copy button to the code blocks. 
+ * This transformer is used to add a header to the code blocks.
  */
-const transformerCopyButton = () => ({
-  name: "transformer-color-lines",
+const transformerCreateCodeBlockHeader = () => ({
+  name: "transformer-create-code-block-header",
   pre(node) {
     const preHeaderDiv = h("div", {
       class: "pre-header",
     });
 
-    const copyCodeButton = h(
-      "div",
-      {
-        class: "wrapper-copy-code"
-      },
-      h(
-        "button",
-        {
-          class: "copy-code",
-          "data-code": this.source,
-          onclick: `
-          navigator.clipboard.writeText(this.dataset.code);
-          this.textContent = 'Copied!';
-          setTimeout(() => this.textContent = 'Copy', 1000)
-        `,
-        },
-        "Copy"
-      )
-    );
-
     node.children.unshift(preHeaderDiv);
+  },
+});
 
+/**
+ * This transformer is used to add a title to the code blocks headers.
+ */
+const transformerAddTitleToCodeBlocksHeaders = () => ({
+  name: "transformer-add-title-to-code-blocks-headers",
+  pre(node) {
     if (this.meta.title) {
-      const titleDiv = h("div", {
-        class: "pre-title",
-      }, this.meta.title);
-      
+      const preHeaderDiv = node.children[0];
+
+      const titleDiv = h(
+        "div",
+        {
+          class: "pre-title",
+        },
+        this.meta.title
+      );
+
       preHeaderDiv.children.push(titleDiv);
     }
+  },
+});
 
+/**
+ * This transformer is used to add a copy button to the code blocks.
+ */
+const transformerCopyButton = () => ({
+  name: "transformer-color-lines",
+  pre(node) {
     if (this.meta.copy) {
+      const preHeaderDiv = node.children[0];
+
+      const copyCodeButton = h(
+        "div",
+        {
+          class: "wrapper-copy-code",
+        },
+        h(
+          "button",
+          {
+            class: "copy-code",
+            "data-code": this.source,
+            onclick: `
+              navigator.clipboard.writeText(this.dataset.code);
+              this.textContent = 'Copied!';
+              setTimeout(() => this.textContent = 'Copy', 1000)
+            `,
+          },
+          "Copy"
+        )
+      );
+
       preHeaderDiv.children.push(copyCodeButton);
     }
   },
@@ -90,14 +110,16 @@ const transformerCopyButton = () => ({
 // https://astro.build/config
 export default defineConfig({
   site: "https://ebacala.com",
-  integrations: [sitemap(), tailwind(), mdx()],
+  integrations: [sitemap(), tailwind()],
   markdown: {
     shikiConfig: {
       theme: "dracula",
       transformers: [
         transformerMeta(),
+        transformerCreateCodeBlockHeader(),
+        transformerAddTitleToCodeBlocksHeaders(),
         transformerCopyButton(),
-        transformerNotationDiff()
+        transformerNotationDiff(),
       ],
       wrap: false,
     },
