97e59d057ecfdfd26211158553a0e9dc0c1ad875
ebacala <apps@ebacala.com>
2026-05-02 22:58:40 +0200

    chore(deps): fixed deprecated from astro

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,22 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 const blog = defineCollection({
   loader: glob({
     pattern: "**/*.md",
     base: "./blog",
   }),
-  schema: ({ image }) => z.object({
-    id: z.string(),
-    title: z.string(),
-    description: z.string(),
-    author: z.string(),
-    publishedDate: z.date(),
-    editedDate: z.date().optional(),
-    ogImage: image().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      id: z.string(),
+      title: z.string(),
+      description: z.string(),
+      author: z.string(),
+      publishedDate: z.date(),
+      editedDate: z.date().optional(),
+      ogImage: image().optional(),
+    }),
 });
 
 export const collections = { blog };
